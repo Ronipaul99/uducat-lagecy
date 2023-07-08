8e4b298baa849571b14ef2e6f33113f67793830a
Reshabcsst <reshabn.csst@gmail.com>
2023-07-08 17:14:20 +0530

    uno

--- src/Components/Schedule/Schedule.tsx
+++ src/Components/Schedule/Schedule.tsx
@@ -4,8 +4,11 @@ import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import TextSnippetOutlinedIcon from '@mui/icons-material/TextSnippetOutlined';
 import SignalCellularAltOutlinedIcon from '@mui/icons-material/SignalCellularAltOutlined';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
 import GroupsOutlinedIcon from '@mui/icons-material/GroupsOutlined';
-import Class from '../Courses/DummyData/Class';
+import Class from '../DummyData/Class';
 
 
 const date1: Date = new Date()
@@ -32,6 +35,7 @@ const Schedule = () => {
   const [classData, setClassData] = useState(initial);
   const [datemap, setDatemap] = useState('');
   const [Timemap, setTimemap] = useState('');
+  const [tab, setTab] = useState(true);
 
 
   useEffect(() => {
@@ -72,11 +76,11 @@ const Schedule = () => {
       <div className="tabfull">
         <div className="joint">
           <div className="tabfit">
-            <button>List of upcoming classes</button>
-            <button>Callander view of events</button>
+            <button onClick={() => setTab(true)}>List of upcoming classes</button>
+            <button onClick={() => setTab(false)}>Callander view of events</button>
           </div>
 
-          <div className="schedulehead">
+          {tab ? <div className="schedulehead">
             <div className="header12">
               <div className="one"><p>Class Tittle</p><TextSnippetOutlinedIcon className='icon' /></div>
               <div className="two"><p>Class ID</p><SignalCellularAltOutlinedIcon className='icon' /></div>
@@ -110,6 +114,22 @@ const Schedule = () => {
             </div>
 
           </div>
+            :
+            <div className="secondTab">
+
+              <div className="calender">
+                <div className="leftc">
+                  <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DateCalendar />
+                  </LocalizationProvider>
+                </div>
+                <div className="mainc">
+
+                </div>
+              </div>
+
+            </div>
+          }
         </div>
       </div>
 
