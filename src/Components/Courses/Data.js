6440a54b23157c4153f5262b1d2cfa45aca8c4ef
Reshabcsst <reshabn.csst@gmail.com>
2023-07-08 13:10:35 +0530

    uno

--- src/Components/Courses/Data.js
+++ src/Components/Courses/Data.js
@@ -2,70 +2,70 @@ const data = [
   {
     "id": "c001",
     "CreatedBy": "Biswarup",
-    "imgURL": "https://static.uacdn.net/thumbnail/course/v2/E9322975-8C2D-4D0F-A51D-D8BCBF2A9F18_special_class.png?q=75&w=640&fm=webp",
+    "imgURL": "https://www.becker.com/sites/default/files/styles/atge_no_style_lg/public/2022-08/What%20to%20expect%20from%20Becker%20CPA%20Live%20Online%20course.jpg?itok=K7wWgRLL",
     "Date": "11/02/2023",
     "details": "This device is form the best Company in the entire world Apple"
   },
   {
     "id": "c002",
     "CreatedBy": "Reshab",
-    "imgURL": "https://static.uacdn.net/thumbnail/course/v2/D3678A2A-BD66-48C0-91F7-1FF279B371E5_special_class.png?q=75&w=384&fm=webp",
+    "imgURL": "https://e2k4h8r9.rocketcdn.me/wp-content/uploads/2022/08/03-candidate-studying.jpg",
     "Date": "11/022023",
     "details": "This device is from the company called Lenovo"
   },
   {
     "id": "c003",
     "CreatedBy": "Joy",
-    "imgURL": "https://static.uacdn.net/thumbnail/course/v2/ABD7E11C-7F44-44FF-9A2D-C5B94182A054_special_class.png?q=75&w=384&fm=webp",
+    "imgURL": "https://www.surgent.com/wp-content/uploads/2022/08/whoweserve-reseller-1.jpg",
     "Date": "11/02/2023",
     "details": "This device is from Dell"
   },
   {
     "id": "c004",
     "CreatedBy": "Asish",
-    "imgURL": "https://static.uacdn.net/thumbnail/course/v2/72BDC43C-F319-40C5-A6AE-B4CE2351E4F6_special_class.png?q=75&w=640&fm=webp",
+    "imgURL": "https://www.b-c-training.com/wp-content/uploads/2022/11/cbci-course.jpg",
     "Date": "11/02/2023",
     "details": "This device is from Very popular company CourseNamed HP"
   },
   {
     "id": "c005",
     "CreatedBy": "Deep",
-    "imgURL": "https://static.uacdn.net/thumbnail/course/v2/B005229F-C0C7-4E1B-850C-479D851CC4C0_special_class.png?q=75&w=640&fm=webp",
+    "imgURL": "https://media.istockphoto.com/id/1419631265/photo/woman-following-online-courses-on-her-laptop-at-home.webp?b=1&s=170667a&w=0&k=20&c=bJwYqtlveob6nFSxJmPSWid9aVOly_mF2PIdjimEWYM=",
     "Date": "11/02/2023",
     "details": "This device is from a company called Acer"
   },
   {
     "id": "c006",
     "CreatedBy": "Ayan",
-    "imgURL": "https://static.uacdn.net/thumbnail/course/v2/1382A161-630D-4368-BDA7-0010906E70C0_special_class.png?q=75&w=384&fm=webp",
+    "imgURL": "https://thumbs.dreamstime.com/b/indian-man-laptop-thinking-home-office-technology-remote-job-lifestyle-concept-computer-190644371.jpg",
     "Date": "11/02/2023",
     "details": "This device is comes from very known company and the company of trus ASUS"
   },
   {
     "id": "c007",
     "CreatedBy": "Suvo",
-    "imgURL": "https://static.uacdn.net/thumbnail/course/v2/98D1FDA8-BF0C-4209-A43D-29B76680E047_special_class.png?q=75&w=384&fm=webp",
+    "imgURL": "https://thumbs.dreamstime.com/b/modern-businessman-thinking-something-sitting-office-modern-businessman-thinking-something-sitting-148709021.jpg",
     "Date": "11/02/2023",
     "details": "This device is from very durable company MSI"
   },
   {
     "id": "c008",
     "CreatedBy": "Argho",
-    "imgURL": "https://static.uacdn.net/thumbnail/course/v2/B0A805F5-8970-41A0-97FA-56B255397232_special_class.png?q=75&w=384&fm=webp",
+    "imgURL": "https://thumbs.dreamstime.com/b/business-people-man-his-office-33238.jpg",
     "Date": "11/02/2023",
     "details": "This device is from very popular HP"
   },
   {
     "id": "c009",
     "CreatedBy": "Ankus",
-    "imgURL": "https://dlcdnwebimgs.asus.com/gain/6136b9b8-d2ce-42b7-9b00-fa1a95a2439f/",
+    "imgURL": "https://images.shiksha.com/mediadata/images/articles/1622527812phpiy6btg.jpeg",
     "Date": "11/02/2023",
     "details": "This device is comes from very known company and the company of trus ASUS"
   },
   {
     "id": "c010",
     "CreatedBy": "Allix",
-    "imgURL": "https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE5apBJ?ver=aff3&q=90&m=8&h=431&w=767&b=%23FFFFFFFF&l=f&x=1065&y=0&s=2135&d=1200&aim=true",
+    "imgURL": "https://cybertex.edu/wp-content/uploads/2023/06/network-engineer-coding-course.jpg",
     "details": "This Course is for Web Developement",
     "Date": "11/02/2023"
   }
