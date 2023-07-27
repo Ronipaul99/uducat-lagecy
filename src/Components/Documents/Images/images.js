df48e010711dd9b20d3ec1af6d0e7be93c11e3ef
Reshabcsst <reshabn.csst@gmail.com>
2023-07-27 12:54:53 +0530

    uno

---
+++ src/Components/Documents/Images/images.js
@@ -0,0 +1,8 @@
+import docs from './icons8-ms-word-30.png';
+import jpg from './icons8-image-30.png'
+const Images = [
+    docs,
+    jpg
+]
+
+export default Images;
--- src/Components/Documents/images.js
+++
@@ -1,8 +0,0 @@
-import docs from './Images/icons8-ms-word-30.png';
-import jpg from './Images/icons8-image-30.png'
-const Images = [
-    docs,
-    jpg
-]
-
-export default Images;
